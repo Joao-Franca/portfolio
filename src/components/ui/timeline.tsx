e4e9de618de256b60f038df7dd642574eb6c6870
João Pedro Ferreira França <161327584+Joao-Franca@users.noreply.github.com>
2025-04-01 15:18:53 -0300

    new

--- src/components/ui/timeline.tsx
+++ src/components/ui/timeline.tsx
@@ -1,10 +1,6 @@
 "use client";
-import {
-  useScroll,
-  useTransform,
-  motion,
-} from "motion/react";
-import React, { useEffect, useRef, useState } from "react";
+
+import React, { useRef } from "react";
 
 interface TimelineEntry {
   title: string;
@@ -12,24 +8,7 @@ interface TimelineEntry {
 }
 
 export const Timeline = ({ data }: { data: TimelineEntry[] }) => {
-  const ref = useRef<HTMLDivElement>(null);
   const containerRef = useRef<HTMLDivElement>(null);
-  const [height, setHeight] = useState(0);
-
-  useEffect(() => {
-    if (ref.current) {
-      const rect = ref.current.getBoundingClientRect();
-      setHeight(rect.height);
-    }
-  }, [ref]);
-
-  const { scrollYProgress } = useScroll({
-    target: containerRef,
-    offset: ["start 10%", "end 50%"],
-  });
-
-  const heightTransform = useTransform(scrollYProgress, [0, 1], [0, height]);
-  const opacityTransform = useTransform(scrollYProgress, [0, 0.1], [0, 1]);
 
   return (
     <div
@@ -38,18 +17,19 @@ export const Timeline = ({ data }: { data: TimelineEntry[] }) => {
     >
       <div className="max-w-7xl mx-auto py-20 px-4 md:px-8 lg:px-10 flex flex-col items-center justify-center">
         <h2 className="text-4xl text-center font-poppins font-bold md:text-7xl mb-4 text-[#809DE9] dark:text-white max-w-4xl">
-        Projects
-      </h2>
-      <p className="text-neutral-700 text-center font-roboto dark:text-neutral-300 text-base md:text-2xl">
-      I have built various different projects to fit different aspects of the client's business. If you want to see more examples of my work than the ones showcased in this site, please{" "}
-      <a href="#contact" className="text-[#809DE9]">
-      contact me!
-      </a>
-</p>
-    </div>
+          Projects
+        </h2>
+        <p className="text-neutral-700 text-center font-roboto dark:text-neutral-300 text-base md:text-2xl">
+          I have built various different projects to fit different aspects of the
+          client's business. If you want to see more examples of my work than the
+          ones showcased in this site, please{" "}
+          <a href="#contact" className="text-[#809DE9]">
+            contact me!
+          </a>
+        </p>
+      </div>
 
-
-      <div ref={ref} className="relative max-w-7xl mx-auto pb-20">
+      <div className="relative max-w-7xl mx-auto pb-20">
         {data.map((item, index) => (
           <div
             key={index}
@@ -68,24 +48,10 @@ export const Timeline = ({ data }: { data: TimelineEntry[] }) => {
               <h3 className="md:hidden block text-2xl mb-4 text-left font-bold text-neutral-500 dark:text-neutral-500">
                 {item.title}
               </h3>
-              {item.content}{" "}
+              {item.content}
             </div>
           </div>
         ))}
-        <div
-          style={{
-            height: height + "px",
-          }}
-          className="absolute md:left-8 left-8 top-0 overflow-hidden w-[2px] bg-[linear-gradient(to_bottom,var(--tw-gradient-stops))] from-transparent from-[0%] via-neutral-200 dark:via-neutral-700 to-transparent to-[99%]  [mask-image:linear-gradient(to_bottom,transparent_0%,black_10%,black_90%,transparent_100%)] "
-        >
-          <motion.div
-            style={{
-              height: heightTransform,
-              opacity: opacityTransform,
-            }}
-            className="h-screen inset-x-0 top-0  w-[2px] bg-gradient-to-t from-[#01F1FE] via-[#809DE9] to-transparent from-[0%] via-[10%] rounded-full"
-          />
-        </div>
       </div>
     </div>
   );
